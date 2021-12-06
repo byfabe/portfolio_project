a51030246ffd77b2dad6593d17a24d987eed43d4
Fabien R <79097567+Zheyn@users.noreply.github.com>
2021-12-06 18:14:34 +0100

    Ajout bio + titre png

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,15 @@
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background: #040210;
+  background-image: url("./assets/texture.png");
+  background-repeat: repeat-y;
+  background-size: cover;
+}
 </style>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,4 +1,26 @@
-<template></template>
+<template>
+  <div class="container-portfolio">
+    <header>
+      <img class="titre-byfabe" src="../assets/byfabe.png" alt="titre byfabe" />
+      <img class="logo-cyber" src="../assets/cyber.png" alt="Logo cyber" />
+    </header>
+    <div class="line"></div>
+    <div class="container-bio">
+      <div class="photo">
+        <img class="photo-profile" src="../assets/profile.png" alt="" />
+      </div>
+      <div class="bio">
+        <p><span class="color-bio">Prénom:</span> Fabien</p>
+        <p><span class="color-bio">Âge:</span> 35 ans</p>
+        <p><span class="color-bio">Localisation:</span> France</p>
+        <p><span class="color-bio">Classe:</span> Développeur web junior</p>
+        <p><span class="color-bio">Caratère:</span> Ça va :)</p>
+        <p><span class="color-bio">Bio:</span> Mon imagination pour seule limite ! </p>
+      </div>
+    </div>
+    <div class="line"></div>
+  </div>
+</template>
 
 <script>
 export default {};
@@ -6,4 +28,76 @@ export default {};
 
 
 <style scoped lang="scss">
+.container-portfolio {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  & .titre-byfabe {
+    width: clamp(250px, 50vw, 1500px);
+    margin-top: 6%;
+  }
+  & .logo-cyber {
+    width: 15vw;
+    height: auto;
+    animation: blink 3s cubic-bezier(0.1, -0.6, 0.2, 0) infinite;
+  }
+}
+@keyframes blink {
+  0%,
+  19.999%,
+  22%,
+  62.999%,
+  64%,
+  64.999%,
+  70%,
+  100% {
+    opacity: 0.2;
+  }
+  20%,
+  21.999%,
+  63%,
+  63.999%,
+  65%,
+  69.999% {
+    opacity: 0.1;
+  }
+}
+.line {
+  width: 20vw;
+  height: 4px;
+  box-shadow: 0 0 20px rgba(255, 39, 183, 0.712),
+    inset 0 0 10px rgba(250, 63, 178, 0.726), 0 2px 0 #000;
+  margin: 5% 0 5% 0;
+}
+.container-bio {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  & .photo-profile {
+    width: clamp(20px, 20vw, 500px);
+    height: auto;
+  }
+  & .bio {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 50vw;
+    & p {
+      font-size: 1.5vw;
+      font-family: "Press Start 2P", cursive;
+      color: #f1f1f1;
+      padding: 20px;
+    }
+  }
+  & .color-bio {
+    color: aqua;
+  }
+}
 </style>
